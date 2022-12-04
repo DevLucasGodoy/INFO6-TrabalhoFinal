c7708b29d8977d0b7aa709bf4daeb7e8a1f18e69
Lucas Godoy <54687280+DevLucasGodoy@users.noreply.github.com>
2022-12-03 23:46:56 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', e => {
+    "use strict";//restrito a funcionar em navegadores comES6 >
+    if("serviceWorker" in navigator){
+        navigator.serviceWorker.register("./sw.js");
+    }
+}); 
---
+++ sw.js
@@ -0,0 +1,36 @@
+let cacheName = "noticias-app";
+let filesToCache = ["/", "./index.html", "./css/style.css", "./js/main.js", "./pages/fallback.html", "./pages/produtos.html"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+    const req = e.request;
+    const url = new URL(req.url);
+    if(url.origin === location.origin) {
+        e.respondWith(cacheFirst(req));
+      }else {
+        e.respondWith(networkFirst(req));
+      }
+});
+
+async function cacheFirst(req) {
+    const cachedResponse = await caches.match(req);
+    return cachedResponse || fetch(req);
+  }
+  
+  async function networkFirst(req) {
+    const cache = await caches.open('projeto-final');
+    try {
+      const res = await fetch(req);
+      cache.put(req, res.clone());
+      return res;
+    } catch(error) {
+      return await cache.match(req);
+    }
+  }
